103f6e45c7ad8708e56f50c31a6e5f3e41b326c5
Kuja <fernandes.jerome03@gmail.com>
2023-02-25 13:00:57 +0100

    Adding some funtionnality to the portfolio page

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,7 +1,7 @@
 import { useSearchParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLink} from "@fortawesome/free-solid-svg-icons";
+import { faStar, faLink } from "@fortawesome/free-solid-svg-icons";
 import data from '../datas/datas.json';
 
 import Error from "../pages/Error";
@@ -27,13 +27,14 @@ function Portfolio(){
             <img className="portfolio__slide" src={projetItem.pictures} alt={projetItem.description}></img>
             <div className="portfolio__container">
                 <p className="portfolio__container__description">{projetItem.description}</p>
-                <ul className="portfolio__container__tagblock"> {projetItem.tags.map((stuff)=>(
-                        <li className="portfolio__container__tagblock__tag" key={stuff}>{stuff}</li>
-                ))}</ul>
-                <Link className="portfolio__container__link" to={projetItem.repo}>
-                    <p>lien vers le repo gitHub</p><FontAwesomeIcon className="portfolio__container__link__icon" icon={faLink} />
-                </Link>
-                
+                <div className="portfolio__container__sider">
+                    <ul className="portfolio__container__sider__tagblock"> {projetItem.tags.map((stuff)=>(
+                            <li className="portfolio__container__sider__tagblock__tag" key={stuff}><FontAwesomeIcon icon={faStar} className="portfolio__container__sider__tagblock__tag__icon"/>{stuff}</li>
+                    ))}</ul>
+                    <Link className="portfolio__container__sider__link" to={projetItem.repo}>
+                    <FontAwesomeIcon icon={faLink} className="portfolio__container__sider__tagblock__tag__icon"/><p>lien vers le repo gitHub</p>
+                    </Link>
+                </div>
             </div>
         </div>
     )
